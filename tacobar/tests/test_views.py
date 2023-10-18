13cae6cf19ed33a6cf10a3e5696b13bfde2003b0
mdmn07C5 <malicdan@protonmail.ch>
2023-10-18 14:21:41 -0400

    added test for menuitem detail url

--- tacobar/tests/test_views.py
+++ tacobar/tests/test_views.py
@@ -1,7 +1,7 @@
 from unittest import skip
 
 from django.test import TestCase, Client
-
+from django.urls import reverse
 from tacobar.models import Category, MenuItem
 from tacobar import views
 
@@ -13,11 +13,17 @@ from tacobar import views
 class TestViewResponses(TestCase):
     def setUp(self):
         self.c = Client()
+        Category.objects.create(name='test', slug='test')
+        self.data1 = MenuItem.objects.create(category_id=1, name='test1', slug='test-dish', price=420.69, image="Nonelmao")
     
     def test_url_allowed_hosts(self):
         """Test allowed hosts
         """
         response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_menuitem_detail_url(self):
+        response = self.c.get(reverse(viewname='tacobar:menuitem_detail',args=['test-dish']))
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_url(self):
