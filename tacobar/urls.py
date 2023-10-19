dc3bac9431ebd2ab050c201808b14f2b52b6fd1f
mdmn07C5 <malicdan@protonmail.ch>
2023-10-19 10:11:01 -0400

    refactor/renamed some urls

--- tacobar/urls.py
+++ tacobar/urls.py
@@ -5,6 +5,6 @@ app_name = 'tacobar'
 
 urlpatterns = [
     path('', views.menu, name='menu'),
-    path('menu/<slug:slug>/', views.menuitem_detail, name='menuitem_detail'),
-    path('search/<slug:category_slug>/', views.category_list, name='category_list'),
+    path('menu-detail/<slug:slug>/', views.menuitem_detail, name='menuitem_detail'),
+    path('<slug:category_slug>/', views.category_list, name='category_list'),
 ]
