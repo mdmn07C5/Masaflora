ceb4910889837b0c76caa49b4d85f15ff5f888aa
mdmn07C5 <malicdan@protonmail.ch>
2023-11-02 15:41:20 -0400

    refactor(catalog:urls) reordered because of error
     - reverse was looking up store slug before menu item slug

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -6,7 +6,7 @@ app_name = 'catalogue'
 urlpatterns = [
     path('', views.menu_all, name='menu_all'),
     path('stores', views.store_all, name='all_stores'),
-    path('<slug:store_slug>', views.store_page, name='store_page'),
     path('<slug:slug>', views.menuitem_detail, name='menuitem_detail'),
+    path('<slug:store_slug>', views.store_page, name='store_page'),
     path('menu/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
