b5eb18491109d298d269473a61410985a5ed10d8
mdmn07C5 <malicdan@protonmail.ch>
2023-11-02 16:07:24 -0400

    test(catalog:views, models) re did tests
     - refactored set up for all test classes as categories require
     stores
     - added tests for stores model
     - added tests for stores views

--- cart/tests/test_views.py
+++ cart/tests/test_views.py
@@ -2,14 +2,15 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
-from catalogue.models import Category, MenuItem
+from catalogue.models import Category, MenuItem, Store
 from account.models import UserBase
 
 class TestCartView(TestCase):
 
     def setUp(self):
         UserBase.objects.create(user_name='admin')
-        Category.objects.create(name='test_category', slug='test-category')
+        Store.objects.create(name='Test Store 1', slug='test-store-1', location='test store 1 location', contact='(332)123-4567', opening_hours = '8:00', closing_hours='10:00')
+        Category.objects.create(name='test_category', slug='test-category', store_id=1)
         MenuItem.objects.create(category_id=1, name='test dish', slug='test-dish', price=1.00, image="Nonelmao")
         MenuItem.objects.create(category_id=1, name='test dish 2', slug='test-dish-2', price=2.00, image="Nonelmao")
         MenuItem.objects.create(category_id=1, name='test dish 3', slug='test-dish-3', price=3.00, image="Nonelmao")
