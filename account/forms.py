c1b8edde3b696de41a0d307ea5f1a6d7076a4c3a
mdmn07C5 <malicdan@protonmail.ch>
2023-11-08 16:06:55 -0500

    added login

--- account/forms.py
+++ account/forms.py
@@ -1,13 +1,19 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from .models import UserBase
 from phonenumber_field.formfields import PhoneNumberField
 
+
 class RegistrationForm(forms.ModelForm):
-    user_name = forms.CharField(label='Enter Username', min_length=4, max_length=50, help_text='Required')
-    email = forms.EmailField(max_length=255, help_text='Required', error_messages={'required': 'Email required.'})
+    user_name = forms.CharField(
+        label='Enter Username', min_length=4, max_length=50, help_text='Required')
+    email = forms.EmailField(max_length=255, help_text='Required', error_messages={
+                             'required': 'Email required.'})
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
-    contact_number = PhoneNumberField(label='Phone Number', help_text='Required', error_messages={'required': 'contact number required.'}, region='US')
+    password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput)
+    contact_number = PhoneNumberField(label='Phone Number', help_text='Required', error_messages={
+                                      'required': 'contact number required.'}, region='US')
 
     class Meta:
         model = UserBase
@@ -18,19 +24,19 @@ class RegistrationForm(forms.ModelForm):
         if UserBase.objects.filter(user_name=user_name).exists():
             raise forms.ValidationError("Username already exists")
         return user_name
-    
+
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords do not match.')
         return cd['password2']
-    
+
     def clean_email(self):
         email = self.cleaned_data['email']
         if UserBase.objects.filter(email=email).exists():
             raise forms.ValidationError("User with that email already exists")
         return email
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['user_name'].widget.attrs.update(
@@ -44,3 +50,10 @@ class RegistrationForm(forms.ModelForm):
         self.fields['contact_number'].widget.attrs.update(
             {'class': 'form-control mb-3', 'placeholder': '123-456-7890'}
         )
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Password', 'id': 'login-pwd'}))
--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import (UserLoginForm)
 
 app_name = 'account'
 
 
 urlpatterns = [
     path('register/', views.account_register, name='register'),
-    path('login/', views.account_login, name='login'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
     # dashboards shit
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
