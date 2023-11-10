b5cc7154044cb9161b85080485b46f4c7aa0e3a4
mdmn07C5 <malicdan@protonmail.ch>
2023-11-09 22:37:16 -0500

    ADD: resetting password
    I pity the more sober me who will undoubtebly wonder what the fuck I did
    and why I did it. He will spend hours tracing the changes and additions
    I made and why it had to be so. He will suffer hours of looking through
    documentation that makes as much sense to him as it does to a rock, as
    he would not know where to begin or what he is looking for. Good luck,
    son.

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 from .models import UserBase
 from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import RegionalPhoneNumberWidget
@@ -62,7 +62,7 @@ class UserLoginForm(AuthenticationForm):
 
 class UserEditForm(forms.ModelForm):
     email = forms.EmailField(
-        label='Account email', max_length=200, widget=forms.TextInput(
+        label='Account email', max_length=255, widget=forms.TextInput(
             attrs={'class': 'form-control mb-3', 'placeholder': 'email', 'id': 'form-email'}))
 
     user_name = forms.CharField(
@@ -70,8 +70,8 @@ class UserEditForm(forms.ModelForm):
             attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'form-user-name', 'readonly': 'readonly'}))
 
     contact_number = PhoneNumberField(
-        region='US', label='contact number', widget=RegionalPhoneNumberWidget(region='US', 
-            attrs={'class': 'form-control mb-3', 'placeholder': '123-456-7890', 'id': 'form-contact-number'}))
+        region='US', label='contact number', widget=RegionalPhoneNumberWidget(region='US',
+                                                                              attrs={'class': 'form-control mb-3', 'placeholder': '123-456-7890', 'id': 'form-contact-number'}))
 
     class Meta:
         model = UserBase
@@ -82,3 +82,25 @@ class UserEditForm(forms.ModelForm):
         self.fields['user_name'].required = True
         self.fields['email'].required = True
         self.fields['contact_number'].required = True
+
+
+class PWResetForm(PasswordResetForm):
+    email = forms.EmailField(max_length=255, widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}
+    ))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not (u := UserBase.objects.filter(email=email).exists()):
+            raise forms.ValidationError("User with that email does not exist.")
+        return email
+    
+class PWResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(label='New password', widget=forms.PasswordInput(
+        attrs={'class':'form-control mb-3', 'placeholder':'New Password', 'id':'form-new-pw'}
+    ))
+
+    new_password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput(
+        attrs={'class':'form-control mb-3', 'placeholder':'Repeat Password', 'id':'form-new-pw-2'}
+    ))
+    # validation done by django
