3d4de2e3170f26142c9504211c0706352d855321
mdmn07C5 <malicdan@protonmail.ch>
2023-11-28 17:07:23 -0500

    added options context processor

--- catalogue/context_processors.py
+++ catalogue/context_processors.py
@@ -1,8 +1,9 @@
-
-from .models import Category
+from .models import Category, Option
 
 
 def categories(request):
-    return {
-        'categories': Category.objects.all()
-    }
+    return {"categories": Category.objects.all()}
+
+
+def options(request):
+    return {"options": Option.objects.all()}
