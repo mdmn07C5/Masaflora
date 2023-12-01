1d552c5487bb4a3f9f16d02112ea38a4610626d9
mdmn07C5 <malicdan@protonmail.ch>
2023-11-30 22:51:01 -0500

    Added description and options below category head
    - also it turns out you can't fucking use css files to
    change color of text, but you can for background color?
    ¯\_(ツ)_/¯

--- cart/cart.py
+++ cart/cart.py
@@ -2,16 +2,15 @@ from catalogue.models import MenuItem
 from decimal import Decimal
 
 
-class Cart():
-    """Session wrapper for Cart model
-    """
+class Cart:
+    """Session wrapper for Cart model"""
 
     def __init__(self, request):
         self.session = request.session
         # self.cart = self.session.get('session_cart', {'Peepee':'PooPoo'})
-        cart = self.session.get('session_cart')
-        if 'session_cart' not in request.session:
-            cart = self.session['session_cart'] = {}
+        cart = self.session.get("session_cart")
+        if "session_cart" not in request.session:
+            cart = self.session["session_cart"] = {}
         self.cart = cart
 
     def save(self):
@@ -25,13 +24,18 @@ class Cart():
         """
         menuitem_id = str(menuitem.id)
         self.cart[menuitem_id] = self.cart.get(
-            menuitem_id, {'price': str(menuitem.price,)})
+            menuitem_id,
+            {
+                "price": str(
+                    menuitem.price,
+                )
+            },
+        )
 
-        qty = self.cart[menuitem_id].get('qty', 0)
-        self.cart[menuitem_id]['qty'] = int(menuitemqty) + int(qty)
+        qty = self.cart[menuitem_id].get("qty", 0)
+        self.cart[menuitem_id]["qty"] = int(menuitemqty) + int(qty)
 
-        self.cart[menuitem_id]['sub_total'] = str(
-            self.get_sub_total(menuitem_id))
+        self.cart[menuitem_id]["sub_total"] = str(self.get_sub_total(menuitem_id))
 
         self.save()
 
@@ -48,26 +52,25 @@ class Cart():
             self.save()
 
     def get_sub_total(self, menuitem_id):
-        return self.cart[menuitem_id]['qty'] * Decimal(self.cart[menuitem_id]['price'])
+        return self.cart[menuitem_id]["qty"] * Decimal(self.cart[menuitem_id]["price"])
 
     def get_total(self):
-        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+        return sum(Decimal(item["price"]) * item["qty"] for item in self.cart.values())
 
     def __len__(self):
-        print(self.cart.values())
-        return sum(item['qty'] for item in self.cart.values())
+        print(self.cart.items())
+        return sum(item["qty"] for item in self.cart.values())
 
     def __iter__(self):
-        """Collect menuitem id from session data to query db and return menuitems
-        """
+        """Collect menuitem id from session data to query db and return menuitems"""
         menuitem_ids = self.cart.keys()
         menuitems = MenuItem.objects.filter(id__in=menuitem_ids)
 
         cart = self.cart.copy()
         for menuitem in menuitems:
-            cart[str(menuitem.id)]['menuitem'] = menuitem
+            cart[str(menuitem.id)]["menuitem"] = menuitem
 
         for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['qty']
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["qty"]
             yield item
